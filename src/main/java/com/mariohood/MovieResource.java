4d73a7c9f9f4e58498459ed7dc6cea8b8f8b66ea
mariohood <manhattan199@hotmail.com>
2023-08-31 20:06:47 -0300

    Primeiro Commit Restfull Web Service com Quarkus

---
+++ src/main/java/com/mariohood/MovieResource.java
@@ -0,0 +1,80 @@
+package com.mariohood;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/movies")
+public class MovieResource {
+    
+    public static List<Movie> movies = new ArrayList<>();
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getMovies() {
+        return Response.ok(movies).build(); 
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/size")
+    public Integer countMovies() {
+        return movies.size();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createMovie(Movie newMovie) {
+        movies.add(newMovie);
+        return Response.ok(movies).build();
+    }
+
+    @PUT
+    @Path("{id}/{title}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateMovie(
+        @PathParam("id") Long id,
+        @PathParam("title") String title) {
+    movies = movies.stream().map(movie -> {
+        if(movie.getId().equals(id)) {
+            movie.setTitle(title);
+        }
+        return movie;
+    }).collect(Collectors.toList());
+    return Response.ok(movies).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response deleteMovie(
+        @PathParam("id") Long id) {
+        Optional<Movie> movieToDelete = movies.stream().filter(movie -> movie.getId().equals(id))
+            .findFirst();
+        boolean removed = false;
+        if(movieToDelete.isPresent()) {
+            removed = movies.remove(movieToDelete.get());
+        }
+        if(removed) {
+            return Response.noContent().build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    
+    }
+    
+}
